b4801deafc139d0d5568fdfcd046c5bf828a85f7
xilosada <xabi@calimero.network>
2025-08-29 14:23:57 +0200

    fix: exclude storybook from CI builds due to vite resolution issues

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -7,9 +7,7 @@ const config: StorybookConfig = {
     "../../packages/charts/src/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs',
-    '@storybook/addon-onboarding'
+    '@storybook/addon-docs'
   ],
   "framework": {
     "name": '@storybook/react-vite',
